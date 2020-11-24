b95c2583ce8f3306b9807ff0cd7b886123111f33
Jasmine Seren Rosso <jasmine.serenrosso@ieseg.fr>
2020-11-24 12:30:18 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #Flashcards is nested in SESSIONS
+  #Categories is nested in WORDS
+  #Favorite is nested in SESSIONS and WORDS
+
+  resources :users, only: [ :show ] do
+    resources :sessions, only: [ :new, :create, :show ] do
+      resources :flashcards, only: [ :new, :create, :index, :show, :edit, :update ]
+      resources :favorites, only: [ :create ]
+    end
+
+    resources :words, only: [ :create, :index, :show, :update, :destroy] do
+      resources :categories, only: [ :create, :update, :destroy ]
+      resources :favorites, only: [ :create ]
+    end
+  end
 end
