b18464b183681cb74d89c0550c63627aa4ca3bc2
Jasmine Seren Rosso <jasmine.serenrosso@ieseg.fr>
2020-11-24 12:57:02 +0100

    user model modified with has_one_attached :photo + all controllers created + dev / prod env modified from local to cloudinary for active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :words
   has_many :categories, through: :words
 
+  has_one_attached :photo
+  
   validates :first_name, :last_name, :mother_tongue, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/, message: "Adresse e-mail non valide", multiline: true }
   validates :password, presence: true, length: { in: 6..8 }
