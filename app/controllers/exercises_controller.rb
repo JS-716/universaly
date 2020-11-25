1ac901b4e0f831a3c7715ea40bd10a31a67baa8e
thomas toulouse <amos-trach@hotmail.fr>
2020-11-25 19:16:00 +0100

    Fonctinnalité flashcard view et create presque terminé

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -4,8 +4,35 @@ class ExercisesController < ApplicationController
   end
 
   def create
-    # creation de la session
-    # creation des flashcards avec les words de la category de la session creer
-    # et avec le nombre de flachcards creer
+    @exercise = Exercise.new(exercise_params)
+    @category = Category.find(params[:exercise][:category])
+    @exercise.category = @category
+    @exercise.user = current_user
+    if @exercise.save
+      @exercise.words_count.times do
+        word = @category.words.sample
+        # reflechir comment ameliorer la logique, eviter les doublons
+        Flashcard.create(word: word, exercise: @exercise)
+      end
+      redirect_to exercise_flashcard_path(@exercise, @exercise.flashcards.first)
+    else
+      render :new
+    end
+  end
+
+  # respond_to do |format|
+  #   if @article.save
+  #     format.html { redirect_to([@magazine,@article], :notice => 'Article was successfully created.') }
+  #     format.xml  { render :xml => @article, :status => :created, :location => @article }
+  #   else
+  #     format.html { render :action => "new" }
+  #     format.xml  { render :xml => @article.errors, :status => :unprocessable_entity }
+  #   end
+  # end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:words_count)
   end
 end
