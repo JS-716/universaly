7c30875ce22e232d3a44bcabb171e502c9d26b10
thomas toulouse <amos-trach@hotmail.fr>
2020-11-29 09:49:36 +0100

    branche cassé sur le css exercice new

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,14 +34,45 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
 });
 
-$(function() {
-	var rangePercent = $('[type="range"]').val();
-	$('[type="range"]').on('change input', function() {
-		rangePercent = $('[type="range"]').val();
-		$('h4').html(rangePercent+'<span></span>');
-		$('[type="range"], h4>span').css('filter', 'hue-rotate(-' + rangePercent + 'deg)');
-		// $('h4').css({'transform': 'translateX(calc(-50% - 20px)) scale(' + (1+(rangePercent/100)) + ')', 'left': rangePercent+'%'});
-    $('h4').css({'transform': 'translateX(-50%) scale(' + (1+(rangePercent/100)) + ')', 'left': rangePercent+'%'});
-    // $('h4').css({'transform': 'translateX(-50%) scale(' + (1+(rangePercent/100)) + ')', 'left': rangePercent+'%'});
-	});
-});
+// $(function() {
+// 	var rangePercent = $('[type="range"]').val();
+// 	$('[type="range"]').on('change input', function() {
+// 		rangePercent = $('[type="range"]').val();
+// 		$('h4').html(rangePercent+'<span></span>');
+// 		$('[type="range"], h4>span').css('filter', 'hue-rotate(-' + rangePercent + 'deg)');
+// 		// $('h4').css({'transform': 'translateX(calc(-50% - 20px)) scale(' + (1+(rangePercent/100)) + ')', 'left': rangePercent+'%'});
+//     $('h4').css({'transform': 'translateX(-50%) scale(' + (1+(rangePercent/50)) + ')', 'left': rangePercent+'%'});
+//     // $('h4').css({'transform': 'translateX(-50%) scale(' + (1+(rangePercent/100)) + ')', 'left': rangePercent+'%'});
+// 	});
+// });
+
+// var $r = $('input[type="range"]');
+// var $ruler = $('<div class="rangeslider__ruler" />');
+
+// // Initialize
+// $r.rangeslider({
+//   polyfill: false,
+//   onInit: function() {
+//     $ruler[0].innerHTML = getRulerRange(this.min, this.max, this.step);
+//     this.$range.prepend($ruler);}
+// });
+
+// function getRulerRange(min, max, step) {
+//   var range = '';
+//   var i = 0;
+  
+//   while (i <= max) {
+//     range += i + ' ';
+//     i = i + step;
+//   }
+//   return range;
+// }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
