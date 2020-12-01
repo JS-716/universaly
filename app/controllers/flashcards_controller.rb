38d6efcefe49a2d9bc3aa924c0ef7ef0f6355324
Jasmine Seren Rosso <jasmine.serenrosso@ieseg.fr>
2020-12-01 15:16:00 +0100

    backend works fine for flashcards#index and create

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -15,14 +15,52 @@ class FlashcardsController < ApplicationController
     end
     # bouton next
     if @flashcard.id ==  @exercise_flashcards[-1].id
-      next_index = @exercise_flashcards.index(@flashcard)
-      @next_flashcard = @exercise_flashcards[next_index]
+      redirect_to exercise_flashcards_path(params[:exercise_id])
     else
       next_index = @exercise_flashcards.index(@flashcard) + 1
       @next_flashcard = @exercise_flashcards[next_index]
     end
   end
 
-  def create
+   #faire un form de flashcards sur l'index avec les champs suivants (fake edit)
+    #requested_position + translated_position pour une flashcard
+      #2 colonnes 
+      #colonne 1 : itération sur flashcards + order_by/sort_by par request_position + afficher le mot requested sous forme de btn (on utilise du CCS avec hover pr faire croire que c'est un btn)
+         #qd on cliquera dessus, du js viendra remplir notre form caché avec la requested_position (data-position = en erb à flashcard.requested_position)
+         #récupérer l'input du form
+         #récupérer dans un tableau, tous les btns de la 1ère colonne (querySelectorAll)
+         #itérer sur ce tableau avec un foreach
+            # mettre un listenner sur chaque btn
+            #au clic, récupérer le dataset.position et l'injecter dans l'input avec innerHTML
+      #colonne 2 : itération sur flashcards + order_by/sort_by par translated_position + afficher le mot translated
+        #idem
+        #soumettre le form
+    #à la dernière étape, cacher ce form, div avec d-none (pour faire disparaitre)
+
+  def index
+   @flashcard = Flashcard.new
+   @exercise = Exercise.find(params[:exercise_id])
+   @flashcards = @exercise.flashcards
   end
+
+  def create
+     #fake update
+      #regarder si requested_position de form = requested position de flashcard
+      #si oui, matching de flashcard = true sinon on ne fait rien
+   @exercise = Exercise.find(params[:exercise_id])
+
+   @flashcard = @exercise.flashcards.find_by(flashcard_params)
+   if @flashcard
+    @flashcard.matching = true
+    @flashcard.save
+   end
+   redirect_to exercise_flashcards_path(@exercise)
+  end
+
+  private
+  def flashcard_params
+   params.require(:flashcard).permit(:requested_position, :translated_position, :matching, :exercise_id, :word_id )
+  end
+    
 end
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,11 +27,13 @@ import "bootstrap";
 import $ from 'jquery';
 
 // Internal imports, e.g:
+import { flashcardMatching } from "../lib/flashcards-index";
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+	flashcardMatching();
 });
 
 $(function() {
