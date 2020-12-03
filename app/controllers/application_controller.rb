7a2b2e1c3b4e4d60ad32d6d069db1c36b6c76763
thomas toulouse <amos-trach@hotmail.fr>
2020-12-03 19:21:54 +0100

    modif2-domain-application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 end
 
 def default_url_options
-  { host: ENV["www.universaly.work/"] || "localhost:8080" }
+  { host: "www.universaly.work" || "localhost:3000" }
   # { host: ENV["www.universaly.work/"] || "localhost:3000" }
   # { host: "localhost:3000" }
 end
